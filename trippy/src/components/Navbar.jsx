a0052b6745f655029e67b7d89eb7b988c8899e65
Ketan Sahu <rorozoro9888@gmail.com>
2023-07-10 18:02:14 +0530

    worked on hamburger menu for responsivness

--- trippy/src/components/Navbar.jsx
+++ trippy/src/components/Navbar.jsx
@@ -1,19 +1,37 @@
 import { Component } from "react";
 import "./NavbarStyles.css";
+import {Link} from "react-router-dom";
+import {MenuItems} from "./MenuItems";
 
 class Navbar extends Component{
+    state = {clicked: false};
+    handleClick = () => {
+        this.setState({clicked: !this.state.clicked});
+    }
     render(){
         return(
             <nav className="NavbarItems">
                 <h1 className="navbar-logo">Trippy</h1>
+
+                <div className="menu-icons" onClick={this.handleClick}>
+                    <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
+                </div>
+                
                 <ul className="nav-menu">
-                    <li>
-                        <Link>
-                           <i></i> Home 
-                        </Link>
-                    </li>
+                    {MenuItems.map((item, index) => {
+                        return(
+                            <li key={index}>
+                                <a className={item.cName} href={item.url}>
+                                <i className={item.icon}></i> {item.title}
+                                </a>
+                            </li>
+                        )
+                    })}
+                    <button>Sign Up</button>
                 </ul>
             </nav>
         )
     }
 }
+
+export default Navbar;
